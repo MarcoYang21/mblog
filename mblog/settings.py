632e48f7fc2a6662888d25d74ff47017460043c6
MarcoYang21 <marco_yang@msn.com>
2024-09-21 16:26:26 +0800

    deploy on RPi4
    change setting for rpi4

--- mblog/settings.py
+++ mblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nhmt4%-l^_v4dqk1t+dt)v4@w5c-yoqsu#hjuq62wn-5zoo40q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR /'static'
 ]
+STATIC_ROOT = '/var/www/mblog/staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
